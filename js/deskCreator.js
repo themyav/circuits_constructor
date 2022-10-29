500f3c3ad9ebbea3d4334222ba4400aff9fc9e7d
themyav <Myavochka119@gmail.com>
2022-10-29 21:18:34 +0300

    Добавлена панель инструментов и возможность добавлять разные элементы

--- js/deskCreator.js
+++ js/deskCreator.js
@@ -1,7 +1,7 @@
 function createDesk(){
     let code = '';
     for(let i = 0; i < 64; i++){
-        code += '<figure><img src=\'resource/desk.png\' alt="Gallery image 1" free=true id=\'img_' + i.toString() + '\' class="gallery__img"></figure>';
+        code += '<figure><img src=\'resource/desk.png\' alt="Gallery image 1" free=true id=\'img_' + i.toString() + '\' rotation = 0 class="gallery__img"></figure>';
     }
     $("#gallery").html(code);
 }
--- js/eventHandler.js
+++ js/eventHandler.js
@@ -1,16 +1,29 @@
+const LAMP = 'lamp';
+const WIRE = 'wire';
+
+var current = 'lamp';
+
 $(document).ready(function (){
-    $('body').on('click', 'img', function (){
+    $('body').on('click', 'img', function (event){
+
+        //console.log(event.which);
 
         let isFree = $(this).attr('free');
+        let rotation = $(this).attr('rotation');
 
         if(isFree === 'false') {
             $(this).attr('src', 'resource/desk.png');
             isFree = 'true';
         }
         else{
-            $(this).attr('src', 'resource/lamp.png');
+            $(this).attr('src', 'resource/' + current.toString() + '.jpg');
             isFree = 'false';
         }
         $(this).attr('free', isFree);
+    });
+
+    $('button').on('click', function (){
+        current = this.id;
+        console.log(current);
     });
 });
