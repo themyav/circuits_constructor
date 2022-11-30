c05aef80611dc02e90b4287d50054e52636cb0da
themyav <Myavochka119@gmail.com>
2022-11-30 22:29:07 +0300

    Добавлена готовая цепь: постоянный ток + последовательное соединение

--- js/deskCreator.js
+++ js/deskCreator.js
@@ -1,6 +1,9 @@
+let M = 10
+let N = 7
+
 function createDesk(){
     let code = '';
-    for(let i = 0; i < 64; i++){
+    for(let i = 0; i < N*M; i++){
         code += '<figure><img src=\'resource/element/desk.png\' alt="Gallery image 1" free=true id=\'img_' + i.toString() + '\' rotation = 1 reflectionX = 1 reflectionY = 1 class="gallery__img"></figure>';
     }
     $("#gallery").html(code);
--- js/eventHandler.js
+++ js/eventHandler.js
@@ -10,12 +10,20 @@ $(document).ready(function (){
 
     //Отлавливаем клик по картинке и всегда ее заменяем на что-то указанной функцией
     $('body').on('click', 'img', function (event){
+        if (event.ctrlKey){
+            if(event.shiftKey){
+                current = "rotate_left";
+            }
+            else current = "rotate_right";
+            action = chooseInstrument;
+        }
         action(this);
     });
 
     // Отлавливаем нажатие на кнопку и выбираем, с каким элементом меню заботаем
     $('button').on('click', function (){
         current = this.id;
+        if(current === '') return;
         let category = document.getElementById(current).closest('table').classList[0];
         let menue = document.getElementById(current).classList[0];
         switch (category){
@@ -76,9 +84,9 @@ if (!event.target.matches('.drop_wires')) {
 
 
 
-
 function circuits_one() {
     console.log("did first");
+
 }
 function circuits_two() {
     console.log("did second");
@@ -86,6 +94,29 @@ function circuits_two() {
 function circuits_three() {
     console.log("did third");
 }
+
+//постоянный ток, последовательное соединение
 function circuits_four() {
     console.log("did fourth");
+    let start = M + Math.round(M/2)
+    let next = M
+    let sheme = [
+        [start, 'corner_wire_4'],
+        [start + 1, 'lamp'],
+        [start + 2, 'corner_wire'],
+        [start + M, 'current_source', rotate_left],
+        [start + M + 2, 'wire_2'],
+        [start + 2*M, 'corner_wire_3'],
+        [start + 2*M + 1, 'wire'],
+        [start + 2*M + 2, 'corner_wire_2'],
+    ]
+    for(let i = 0; i < sheme.length; i++){
+        current = sheme[i][1]
+        console.log(sheme[i][0])
+        let cell = document.getElementById("img_" + sheme[i][0])
+        chooseElement(cell)
+        if(sheme[i].length > 2) sheme[i][2](cell)
+    }
+
+
 }
