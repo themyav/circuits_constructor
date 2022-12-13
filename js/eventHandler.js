4fb17cc33ded5394ae4cacbc028eb61baed4aa71
themyav <Myavochka119@gmail.com>
2022-12-13 21:05:56 +0300

    Немного исправлен дизайн + баг с отображением картинок у кнопок справочника

--- js/eventHandler.js
+++ js/eventHandler.js
@@ -46,7 +46,8 @@ function handleBuildModeButton(cell){
 
     removeButtonsLighting();
     //отсекаем рабочие кнопки
-    if(cell.id === '' || cell.id === 'scale_plus' || cell.id === 'scale_minus') return;
+    if(cell.id === '' || cell.id === 'scale_plus' || cell.id === 'scale_minus'
+    || cell.id === 'project_description' || cell.id === 'elements_description' || cell.id === 'instruction') return;
     current = cell.id;
     document.getElementById(current).style.filter = 'brightness(50%)';
     let category = document.getElementById(current).closest('table').classList[0];
